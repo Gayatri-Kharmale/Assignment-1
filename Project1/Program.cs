3fbd4b5c79b6a0054ac6175fbaf81dc781563e4a
Gayatri-Kharmale <gayatrikharmale123@gmail.com>
2022-04-07 10:53:56 +0530

    first commit

--- Project1/Program.cs
+++ Project1/Program.cs
@@ -1,2 +1,238 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//int a = 10;
+//float f = 67.76f;
+//double d = 56.98;
+//string str = "gayatri";
+
+
+
+
+//Console.WriteLine("value of a="+a);
+//Console.WriteLine("value of f{0},d={1}",f,d);
+//Console.WriteLine($"a={a},float={f},double={d}");
+///Console.WriteLine(str);
+
+
+//-------------------------------------------------------------------------------------------------------------
+
+//write a program to check number is Even or odd.
+
+
+//Console.WriteLine("enetr number");
+//int i=Convert.ToInt32(Console.ReadLine());
+
+//if(i%2==0)
+//{
+//    Console.WriteLine("i is even");
+//}
+//else
+//{
+//    Console.WriteLine("i is odd");
+//}
+
+//-------------------------------------------------------------------------------------------------------------
+
+
+//write a program to check positive of negative
+//Console.WriteLine("enetr number");
+//int i = Convert.ToInt32(Console.ReadLine());
+
+//if(i<0)
+//{
+//    Console.WriteLine("i is negative");
+//}
+//else
+//{
+//    Console.WriteLine("i is positive");
+//}
+
+
+
+
+
+////--------------------------------------------------------------------------------------------------------
+
+//write a program to print sum of digit
+
+//Console.WriteLine("enetr number");
+//int num = Convert.ToInt32(Console.ReadLine());
+//int sum = 0;
+//while(num>0)
+//{
+//    int rem = num % 10;
+//    sum = sum + rem;
+//    num = num / 10;
+//Console.WriteLine("sum of digit="+sum);
+
+////--------------------------------------------------------------------------------------------------------
+//write program to calculate power
+
+//Console.WriteLine("enetr base");
+//int b = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("enetr exponant");
+//int expo = Convert.ToInt32(Console.ReadLine());
+//int power = 1;
+//int i = 1;
+//while(i<=expo)
+//{
+//    power = power * b;
+// i++;
+///}
+//Console.WriteLine(power);
+
+
+
+////--------------------------------------------------------------------------------------------------------
+//Console.WriteLine("enetr num1");
+//int num1 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("enetr num2");
+//int num2 = Convert.ToInt32(Console.ReadLine());
+//if(num1>num2)
+//{
+//    Console.WriteLine(num1+"is largest number");
+//}
+//else
+//{
+//    Console.WriteLine(num2+"is largest number");
+//}
+
+////--------------------------------------------------------------------------------------------------------
+// write program to blood donate or not
+//Console.WriteLine("enetr age");
+//int age = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("enetr weight");
+//int weight = Convert.ToInt32(Console.ReadLine());
+//if(age>18)
+//{
+//    if(weight>50)
+//    {
+//        Console.WriteLine("valid for blood donate");
+//    }
+//}
+//else
+//{
+//    Console.WriteLine("not valid");
+//}
+
+
+
+
+////--------------------------------------------------------------------------------------------------------
+///write a program to check greatest of 3 numbers
+//Console.WriteLine("enetr first number");
+//int a = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("enetr second number");
+//int b = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("enetr third number");
+//int c = Convert.ToInt32(Console.ReadLine());
+//if (a >= b)
+//{
+
+//    if(a>=c)
+//    {
+//        Console.WriteLine("A is greater="+a);
+//    }
+//    else
+//    {
+//        Console.WriteLine("C is greater="+c);
+//    }
+
+
+//}
+//else
+//{
+//    if(b>=c)
+//    {
+//        Console.WriteLine("B is greater="+b);
+//    }
+//    else
+//    {
+//        Console.WriteLine("C is greater="+c);
+//    }
+//}
+
+
+
+
+//-------------------------------------------------------------------------------------------------------------
+//write program to find sum of 1 to 5 number
+//int sum = 0;
+//for(int i=1;i<=5;i++)
+//{
+//    sum = sum + i;
+//}
+//Console.WriteLine(sum);
+
+
+
+//-------------------------------------------------------------------------------------------------------------
+//write program to calculate factorial
+
+//int fact = 1;
+//for (int i = 1; i <= 5; i++)
+//{
+//    fact = fact * i;
+//}
+//Console.WriteLine(fact);
+
+
+//-------------------------------------------------------------------------------------------------------------
+//write a preogram to chech number is pallindron or not
+//Console.WriteLine("enetr number");
+//int num = Convert.ToInt32(Console.ReadLine());
+//int temp = num;
+//int reverse = 0;
+//while(num > 0)
+//{
+//    int r = num % 10;
+//    reverse = reverse * 10 + r;
+//    num = num/10;
+//}
+//Console.WriteLine(reverse);
+//num = temp;
+//if(num==reverse)
+//{
+//    Console.WriteLine(" number is pallindrom");
+//}
+//else
+//{
+//    Console.WriteLine("number is not pallindrom");
+//}
+
+//-------------------------------------------------------------------------------------------------------------
+
+//write program to create simple calculator
+
+
+
+    //Console.WriteLine("enetr first number");
+    //int num1 = Convert.ToInt32(Console.ReadLine());
+    //Console.WriteLine("enetr second number");
+    //int num2 = Convert.ToInt32(Console.ReadLine());
+    //Console.WriteLine("1.Addition \n 2.Subtraction \n 3.multiplication \n 4.division");
+    //Console.WriteLine("enter your choice:");
+    //int choice = Convert.ToInt32(Console.ReadLine());
+    //switch (choice)
+    //{
+    //    case 1:
+    //        Console.WriteLine("Addition=" + (num1 + num2));
+    //        break;
+    //    case 2:
+    //        Console.WriteLine("Substraction="+(num1 - num2));
+    //        break;
+    //    case 3:
+    //        Console.WriteLine("multiplication="+(num1*num2));
+    //        break;
+    //    case 4:
+    //        Console.WriteLine("division=" + (num1 / num2));
+    //        break;
+    //    default:
+    //        Console.WriteLine("Invalid choice");
+    //    break;
+
+
+    //}
+
+
+
